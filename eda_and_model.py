a3a2b5783b10c9609fe7a3384facd7f6d95d78a7
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2023-11-23 15:41:04 +0100

    first commit

---
+++ eda_and_model.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+if __name__ == '__main__':
+    # Exploratory Data Analysis go here
+
+    # %% Take advantage of Jupyter notebook cells!
+    # This is one cell
+    pass
+
+    # %% This is another cell
+    pass
---
+++ xai.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+if __name__ == '__main__':
+    # Explainable AI tools go here
+
+    # %% Take advantage of Jupyter notebook cells!
+    # This is one cell
+    pass
+
+    # %% This is another cell
+    pass
