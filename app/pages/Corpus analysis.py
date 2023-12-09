fe7ff5d5b070317c89ced64ce57dc3c25a48cee2
marcagve18 <78203527+marcagve18@users.noreply.github.com>
2023-12-09 16:02:32 +0100

    EDA finished

--- app/pages/Corpus analysis.py
+++ app/pages/Corpus analysis.py
@@ -7,6 +7,11 @@ import numpy as np
 from wordcloud import WordCloud
 import spacy
 from collections import Counter
+import datasets
+
+@st.cache_data
+def load_dataset():
+    return datasets.load_from_disk("data/tokenized_datasets/sampled")
 
 @st.cache_data
 def load_data():
@@ -17,6 +22,34 @@ def load_data():
 
     return df, df_without_outliers
 
+@st.cache_data
+def plot_wordcloud():
+    text = ""
+    for idx, row in df_plot.iterrows():
+        text += f" {row['text']}"
+    fig = plt.figure()
+    wordcloud = WordCloud(background_color="white").generate(text[0:1000000])
+    plt.axis("off")
+    plt.imshow(wordcloud, interpolation='bilinear')
+
+    st.pyplot(fig)
+
+@st.cache_data
+def plot_ner():
+    text = ""
+    for idx, row in df_plot.iterrows():
+        text += f" {row['text']}"
+
+    nlp = spacy.load("en_core_web_sm")
+    results = nlp(text[0:1000000])
+    labels = [x.label_ for x in results.ents]
+    counter = Counter(labels)
+    count=counter.most_common()
+    x,y=map(list,zip(*count))
+    fig = plt.figure()
+    sns.barplot(x=y,y=x, hue=x)
+    st.pyplot(fig)
+
 def print_description(toggled_description, nt_description, is_toggled):
     if is_toggled:
         st.write(toggled_description)
@@ -25,26 +58,34 @@ def print_description(toggled_description, nt_description, is_toggled):
 
 if __name__ == '__main__':
 
-    st.header("Corpus Analysis")
-
+    st.header("Corpus Analysis 🔎")
     df_outliers, df_without_outliers = load_data()
    
-    is_toggled = st.toggle("Remove outliers", value=True)
+    st.warning(
+        """
+        ⚠️ By toggling **Remove outliers** some of the plots and their corresponding analysis will change.\n\n
+        Have fun :) !
+        """
+    )
+
+    is_toggled = st.sidebar.toggle("Remove outliers", value=True)
 
     if not is_toggled:
         df_plot = df_outliers
     else:
         df_plot = df_without_outliers
 
+    st.divider()
+    st.subheader("Distribution of our two classes")
     fig = plt.figure()
     grouped = df_plot.groupby("label").count().values
     data = [grouped[0,0], grouped[1,0]]
     colors = sns.color_palette('pastel')[0:2]
-    plt.pie(data, labels = ["Human", "AI"], colors = colors, autopct='%.0f%%')
+    plt.pie(data, labels = ["Human ≡ 0", "AI ≡ 1"], colors = colors, autopct='%.0f%%')
     st.pyplot(fig)
     print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
+        "As it can be observed, the proportion of human written essays in our dataset is slightly higher than the AI written ones.", 
+        "As it can be observed, the proportion of human written essays in our dataset is slightly higher than the AI written ones.", 
         is_toggled
     )
     st.divider()
@@ -55,20 +96,16 @@ if __name__ == '__main__':
     sns.histplot(data=df_plot, x="word_count", kde=True, hue="label")
     with tab1: 
         st.pyplot(fig)
-    print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        is_toggled
-    )
 
     fig = plt.figure()
     sns.boxplot(data=df_plot, y="word_count", hue="label")
     with tab2:
         st.pyplot(fig)
-    print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        is_toggled
+
+    st.write("""
+        The number of words per essay seems to follow in both cases a Poission distribution. There is not a significative difference between both distributions apart from the skeweness. The human written essays tend to be larger than the AI generated ones.\n\n
+        This does not give us any special insight since the length of the AI essay could be larger, there is not a limit on the maximum length that an AI essay can generate. Therefore, we can affirm that this effect is due to the nature of this specific dataset.
+        """
     )
     st.divider()
 
@@ -82,19 +119,20 @@ if __name__ == '__main__':
     sns.histplot(data=df_plot, x="avg_word_len", kde=True, hue="label", bins=bins)
     with tab1:
         st.pyplot(fig)
-    print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        is_toggled
-    )
 
     fig = plt.figure()
     sns.boxplot(data=df_plot, y="avg_word_len", hue="label")
     with tab2:
         st.pyplot(fig)
     print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
+        """
+        As we can observe there is a substantial difference in the average word length between the two classes. Both distributions are Normal. Despite there is a clear overlap between both of them, AI generated essays clearly have a higher word length than human ones.\n\n
+        This can be clearly seen in the boxplot, where the overlap between the 25% and 75% quantiles is almost negligible. If we were to develop a model to classify them based on *"computed metrics"*, the average word length would be a determining variable.  
+        """,
+        """
+        The presence of outliers does not let us analyze the distribution of the average word length. We can see that there is some non-overlapping zone, but we have to remove the outliers to do a better analysis.
+        """ 
+        , 
         is_toggled
     )
 
@@ -105,42 +143,71 @@ if __name__ == '__main__':
     sns.scatterplot(data=df_plot, x="word_count", y="avg_word_len", hue="label")
     st.pyplot(fig)
     print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
+        """
+        When we compare the two previously seen features, we can observe some pattern that we could already expect from the individual analysis. AI generated essays tend to have a higher average word length for both long and short essays. On the other hand, human written essays have a lower average word length for all text lenghts.
+        \n\n
+        It can also be observed that while AI generated essays have a higher variance in terms of average word length, human ones in general do not reach very high values.
+        \n\n
+        As a general conclusion, we can observe that a predictive model based on these two features would not be very precise since there is only a feature that differentiates the two classes, and not very clearly.
+        """,
+        """
+        At first sight we can observe that there is some kind of split between AI and human essays. Human ones are longer and with a lower average word length. On the other hand, AI ones are shorter and with longer words. 
+        \n\n
+        By removing the outliers we can take a closer look to the data to check more in detail whether this behaviour also happens at a lower scale.
+        """,
         is_toggled
     )
 
     st.divider()
     st.subheader("Most frequent words")
-    text = ""
-    for idx, row in df_plot.iterrows():
-        text += f" {row['text']}"
-    fig = plt.figure()
-    wordcloud = WordCloud(background_color="white").generate(text)
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    plt.show()
-
-    st.pyplot(fig)
-    print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        is_toggled
+    st.warning(
+        """
+        ⚠️ The following plots have been performed from a **sample of our training data of 10.000 essays**. The high computation requirements along with the length of the texts results into a very time consuming process to do the plots. This is why a sampled subset of our data has been taken.
+        """
+    )
+    plot_wordcloud()
+    st.write(
+        """
+        From the wordcloud we can infer which is the main topic of the training essays. The most repeated terms are *student, school, people, help*..., which gives us a hint that most of the essays are related whith academic topics.
+        """     
     )
 
+    st.divider()
+    st.subheader("Most popular entity types")
+    plot_ner()
+    st.write(
+        """
+        As it can be observed, the most frequent type of terms are organizations, then person names and then cardinal numbers. This does not imply that they must appear in the wordcloud, since there can be many of these words but with a low frequency.
+        """     
+    )   
+
+    st.write("Type the entity above to see a more detailed description")
+    entity_type = st.text_input("Entity type")
+    st.write(f"Description: *{spacy.explain(entity_type)}*")
+
     nlp = spacy.load("en_core_web_sm")
-    results = nlp(text[0:1000000])
-    labels = [x.label_ for x in results.ents]
-    counter = Counter(labels)
-    count=counter.most_common()
-    x,y=map(list,zip(*count))
+    st.write("Type a word to check its type")
+    word = st.text_input("Word")
+    processed_word = ""
+    if len(word)>0:
+        processed_word = nlp(word.strip())[0].tag_
+    st.write(f"Description: *{processed_word}*")
+
+    st.divider()
+    st.subheader("TSNE Visualization")
+
+    tsne_datasets = load_dataset()
+    embeddings_tsne_0 = tsne_datasets['embedding_tsne']
+    embeddings_tsne_1 = tsne_datasets['embedding_tsne_1']
     fig = plt.figure()
-    sns.barplot(x=y,y=x, hue=x)
+    sns.scatterplot(x=embeddings_tsne_0, y=embeddings_tsne_1, hue=tsne_datasets['label'])
     st.pyplot(fig)
-    print_description(
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        "As it can be observed, the proportion of human written essays is slightly higher than the AI written ones.", 
-        is_toggled
+
+    st.write(
+        """
+        We can observe that we can split easily the human and AI generated essays after performing TSNE on their embeddings. This leads us to the conclusion that our data is quite 'artificial'.\n\n
+        In the real world this difference is not that clear. So, this training data we have will in fact help the model to differentiate between the two classes in purpose, but not by nature. 
+        """
     )
 
 
--- src/sample_data.py
+++ src/sample_data.py
@@ -1,6 +1,20 @@
 # %% Imports
 import pandas as pd
 import numpy as np
+from transformers import pipeline
+import os
+import datasets
+from utils import get_torch_device
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+def embed(data):
+  e = embedder(data['text'])
+  n = np.array(e)
+  mean_array = np.squeeze(np.mean(n, axis=1))
+    
+  return {"embedding": mean_array}
+
 
 # %% DATA LOADING
 if __name__ == '__main__':
@@ -15,4 +29,41 @@ if __name__ == '__main__':
     df.to_csv("./../data/data_sample.csv")
 
 
-# %%
+    # %% EMBEDDING MODEL LOADING
+    DATA_FOLDER = '../data'
+    MODEL_FOLDER = '../model'
+
+    model_path = os.path.abspath(MODEL_FOLDER)
+    embedder = pipeline(
+        'feature-extraction',
+        model=model_path,
+        device=get_torch_device(debug=True),
+        truncation=True,
+        padding=True,
+        top_k=1,
+    )
+    # %% EMBEDDING SAMPLE TEXTS : This is computationally expensive so we recommend
+    #    to run it on a GPU
+    dataset = datasets.Dataset.from_pandas(df)
+    print("Embedding sample dataset...")
+    dataset = dataset.map(embed)
+    print("Embedding ended")
+        
+    # %% TSNE
+    text_embeddings = np.array(dataset['embedding'])
+    tsne = TSNE(n_components=2, random_state=42)
+    embeddings_tsne = tsne.fit_transform(text_embeddings)
+
+    # We persist the vectors after the TSNE to improve the streamlit app performance.
+    dataset = dataset.add_column("embedding_tsne", embeddings_tsne[:, 0])
+    dataset = dataset.add_column("embedding_tsne_1", embeddings_tsne[:, 1])
+    dataset.save_to_disk(f"{DATA_FOLDER}/tokenized_datasets/sampled")
+
+    # %% VISUALIZATION
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(embeddings_tsne[:, 0], embeddings_tsne[:, 1], c = dataset['label'])
+    legend1 = ax.legend(*scatter.legend_elements(), loc="best", title="Classes")
+    plt.title('t-SNE Visualization of Text Embeddings')
+    ax.add_artist(legend1)
+    plt.show()
+
