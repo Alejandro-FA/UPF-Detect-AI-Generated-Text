38b658444f212cfd9d60fefd5f514d035e22b1cf
Alejandro-FA <61871580+Alejandro-FA@users.noreply.github.com>
2023-12-09 12:02:12 +0100

    Evaluation performed for both models (ours and roberta)

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,6 +1,7 @@
 # %% Imports and helper functions
 from utils import get_torch_device, plot_confusion_matrix
 
+
 if __name__ == '__main__':
     from transformers import pipeline
     from transformers.pipelines.pt_utils import KeyDataset
@@ -19,10 +20,10 @@ if __name__ == '__main__':
     DATA_FOLDER = '../data'
     MODEL_FOLDER = '../model'
 
-    # %% Load AI-generatet text classification model
+    # %% Load AI-generatet text classification models
     model_path = os.path.abspath(MODEL_FOLDER)
-    classifier = pipeline(
-        'text-classification',
+    classifier_ours = pipeline(
+        task='text-classification',
         model=model_path,
         device=get_torch_device(debug=True),
         truncation=True,
@@ -30,55 +31,102 @@ if __name__ == '__main__':
         top_k=1,
     )
 
-    # %% Load test dataset
-    tokenized_datasets = datasets.load_from_disk(f'{DATA_FOLDER}/tokenized_datasets/new')
-    data = KeyDataset(tokenized_datasets['test'], 'text')
-    id2label = {0: "human", 1: "ai"}
-    label2id = {"human": 0, "ai": 1}
+    classifier_roberta = pipeline(
+        task="text-classification",
+        model="Hello-SimpleAI/chatgpt-detector-roberta",
+        device=get_torch_device(debug=True),
+        truncation=True,
+        padding=True,
+        top_k=1,
+    )
 
-    # %% Load validation dataset
-    df = pd.read_json(f'{DATA_FOLDER}/all.jsonl', lines=True).drop(columns='index')
+    classifiers = [
+        {
+            'classifier': classifier_ours,
+            'label2id': {"human": 0, "ai": 1},
+            'name': 'ours',
+        },
+        {
+            'classifier': classifier_roberta,
+            'label2id': {"Human": 0, "ChatGPT": 1},
+            'name': 'roberta',
+        },
+    ]
 
-    df = df[df['human_answers'].str.len() >= 1]
-    df = df[df['chatgpt_answers'].str.len() >= 1]
-    df['human_answers'] = df['human_answers'].str.get(0)
-    df['chatgpt_answers'] = df['chatgpt_answers'].str.get(0)
+
+    # %% Create validation dataset
+    human = (
+        pd.read_json(f'{DATA_FOLDER}/CHEAT/ieee-init.jsonl', lines=True)
+        .drop(columns=['id', 'title', 'keyword'])
+        .rename(columns={'abstract': 'text'})
+        .sample(n=750, random_state=42)
+    )
+    human['label'] = 0
+
+    ai = (
+        pd.read_json(f'{DATA_FOLDER}/CHEAT/ieee-chatgpt-generation.jsonl', lines=True)
+        .drop(columns=['id', 'title', 'keyword'])
+        .rename(columns={'abstract': 'text'})
+        .sample(n=750, random_state=42)
+    )
+    ai['label'] = 1
+
+    mgt = pd.read_csv(f'{DATA_FOLDER}/MGTBench/TruthfulQA_LLMS.csv').dropna()
+    human2 = pd.DataFrame({'text': mgt['Best Answer'], 'label': 0})
+    ai2 = pd.DataFrame({'text': mgt['ChatGPT-turbo_answer'], 'label': 1})
+
+    df = pd.concat([human, ai, human2, ai2]).reset_index(drop=True)
     df.info()
+    df.head()
 
-    # Convert dataframe to our use-case
-    human = pd.DataFrame({'text': df['human_answers'], 'label': 0})
-    ai = pd.DataFrame({'text': df['chatgpt_answers'], 'label': 1})
-    concat_df = pd.concat([human, ai])
-    all_texts_datasets = datasets.Dataset.from_pandas(concat_df)
-    data = KeyDataset(all_texts_datasets, 'text')
-    concat_df.head(5)
+
+    # %% Create Huggingface dataset
+    dataset = datasets.Dataset.from_pandas(df)
+    key_dataset = KeyDataset(dataset, 'text')
+
 
     # %% Load or create predictions
-    try:
-        y_true = np.load(f'{DATA_FOLDER}/y_true2.npy')
-        y_pred = np.load(f'{DATA_FOLDER}/y_pred2.npy')
-    except:
-        outputs = classifier(data, batch_size=32)
-        y_true = np.array( [x['label'] for x in all_texts_datasets], dtype=np.bool_ )
-        # y_true = np.array( [x['label'] for x in tokenized_datasets['test']], dtype=np.bool_ )
-        y_pred = np.zeros(len(data), dtype=np.bool_)
+    all_y_true = [None] * len(classifiers)
+    all_y_pred = [None] * len(classifiers)
 
-        for i, out in tqdm(enumerate(outputs), total=len(data)):
-            y_pred[i] = label2id[out[0]['label']]
-            i += 1
+    for i, pipe in enumerate(classifiers):
+        classifier = pipe['classifier']
+        name = pipe['name']
+        label2id = pipe['label2id']
 
-        np.save(f'{DATA_FOLDER}/y_true2.npy', y_true)
-        np.save(f'{DATA_FOLDER}/y_pred2.npy', y_pred)
+        try:
+            y_true = np.load(f'{DATA_FOLDER}/y_true_{name}.npy')
+            y_pred = np.load(f'{DATA_FOLDER}/y_pred_{name}.npy')
+        except:
+            outputs = classifier(key_dataset, batch_size=32)
+            y_true = np.array( [x['label'] for x in dataset], dtype=np.bool_ )
+            y_pred = np.zeros(len(dataset), dtype=np.bool_)
+
+            for j, out in tqdm(enumerate(outputs), total=len(dataset)):
+                y_pred[j] = label2id[out[0]['label']]
+                j += 1
+
+            np.save(f'{DATA_FOLDER}/y_true_{name}.npy', y_true)
+            np.save(f'{DATA_FOLDER}/y_pred_{name}.npy', y_pred)
+        finally:
+            all_y_true[i] = y_true
+            all_y_pred[i] = y_pred
+
 
     # %% Evaluate performance
-    accuracy = accuracy_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred, average='macro')
-    cm = confusion_matrix(y_true, y_pred, normalize='true')
+    for i, pipe in enumerate(classifiers):
+        print(f'Performance of model: {pipe["name"]}')
 
-    print(f'\nAccuracy: {accuracy:.4f}')
-    print(f'\nf1 score: {f1:.4f}')
-    print('\nClassification report:')
-    print(classification_report(y_true, y_pred, target_names=label2id.keys(), digits=4))
+        accuracy = accuracy_score(all_y_true[i], all_y_pred[i])
+        f1 = f1_score(all_y_true[i], all_y_pred[i], average='macro')
+        cm = confusion_matrix(all_y_true[i], all_y_pred[i], normalize='true')
 
-    plot_confusion_matrix(cm, classes=label2id.keys(), figsize=(8, 6), is_norm=True)
+        print(f'\nAccuracy: {accuracy:.4f}')
+        print(f'\nf1 score: {f1:.4f}')
+        print('\nClassification report:')
+        print(classification_report(all_y_true[i], all_y_pred[i], target_names=label2id.keys(), digits=4))
+
+        plot_confusion_matrix(cm, classes=label2id.keys(), figsize=(8, 6), is_norm=True)
     
+
+# %%
